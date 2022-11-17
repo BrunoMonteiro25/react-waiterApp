ec80afbe13dfb209993eedb57af19916fcdf5b29
BrunoMonteiro25 <bruno.monteiro.silva25@gmail.com>
2022-11-17 02:42:29 -0300

    Modal

--- src/components/OrdersBoard/index.tsx
+++ src/components/OrdersBoard/index.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react'
 import { Order } from '../../types/Order'
+import { OrderModal } from '../OrderModal'
 import { Board, OrdersContainer } from './styles'
 
 interface OrdersBoardProps {
@@ -8,10 +10,26 @@ interface OrdersBoardProps {
 }
 
 export function OrdersBoard({ icon, title, orders }: OrdersBoardProps) {
-  function handleOpenModal() {}
+  const [isModalVisible, setIsModalVisible] = useState(false)
+  const [selectedOrder, setSelectedOrder] = useState<null | Order>(null)
+
+  function handleOpenModal(order: Order) {
+    setIsModalVisible(true)
+    setSelectedOrder(order)
+  }
+
+  function handleCloseModal() {
+    setIsModalVisible(false)
+    setSelectedOrder(null)
+  }
 
   return (
     <Board>
+      <OrderModal
+        visible={isModalVisible}
+        order={selectedOrder}
+        onClose={handleCloseModal}
+      />
       <header>
         <span>{icon}</span>
         <strong>{title}</strong>
@@ -21,7 +39,11 @@ export function OrdersBoard({ icon, title, orders }: OrdersBoardProps) {
       {orders.length > 0 && (
         <OrdersContainer>
           {orders.map((order) => (
-            <button onClick={handleOpenModal} type="button" key={order._id}>
+            <button
+              onClick={() => handleOpenModal(order)}
+              type="button"
+              key={order._id}
+            >
               <strong>Mesa {order.table}</strong>
               <span>{order.products.length} itens</span>
             </button>
